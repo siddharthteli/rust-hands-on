9e155bc5a2c1416ab21cc80942b5b00ecc11c212
Siddharth Teli <telisiddharth123@gmail.com>
2022-06-14 23:45:20 +0530

    Add: String reference, mut reference & move.
    Implemented-
    - Method with string as input taking ownership
    - Methid with refernce as input, can read it.
    - Method with mut refernce as as input, can read & update value.
    
    Because-

---
+++ mod_8/README.md
@@ -0,0 +1,10 @@
+# String
+- Moveing value
+  - If we pass a string value to a function/variable that original variable is not valid to use anyone
+
+- Reference
+ - If we do not want functions or other variables to own a value we can assign them refernce type.
+ - It doesn't take ownership & creates new ptr to stack ptr.
+
+- Mut reference
+ - This types value can be updated.
---
+++ mod_8/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // println!("Value : {}", return_string("sidD".to_string()));
+    let data: String = "Siddharth".to_string();
+    let mut data1: String = "Siddharth".to_string();
+    
+    return_string_reference(&data);
+    println!("{}", data);
+}
+
+fn return_string(data: String) -> String {
+    let mut result = data.split_ascii_whitespace();
+    result.next().unwrap().to_string()
+}
+
+fn return_string_reference(data: &String) -> String {
+    let mut result = data.split_ascii_whitespace();
+    result.next().unwrap().to_string()
+}
+
+fn takes_mut_reference(data: &mut String) {}
+
+
+fn test() {
+    let test = String::from("Siddharth");
+    let test1 = test.clone();
+    println!("Value of string {}", test);
+}
